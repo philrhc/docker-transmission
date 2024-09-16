df5b68d4ce85300935ba4346ae77ecb4dc9b66de
Phil Cummins <philip.cummins@bsc.es>
2024-09-16 09:48:43 +0200

    gets magnet link for dummy torrent

--- seed-dummy-torrent.sh
+++ seed-dummy-torrent.sh
@@ -1,3 +1,4 @@
 echo "hello cluster" >> /var/tmp/docker_transmission_mount/downloads/complete/hello-world.txt;
-transmission-create hello-world.txt
-transmission-remote localhost -a ./hello-world.txt.torrent
+transmission-create /var/tmp/docker_transmission_mount/downloads/complete/hello-world.txt
+transmission-remote localhost -a /var/tmp/docker_transmission_mount/downloads/complete/hello-world.txt.torrent
+transmission-show -m /var/tmp/docker_transmission_mount/downloads/complete/hello-world.txt.torrent
