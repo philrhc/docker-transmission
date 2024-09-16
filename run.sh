4846735859915f697c12a8a5e8cf8fbbb2e65283
Phil Cummins <philip.cummins@bsc.es>
2024-09-16 09:10:05 +0200

    adds build and run scripts, removes DHT and PEX

---
+++ run.sh
@@ -0,0 +1,19 @@
+docker run -d \
+  --name=transmission \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=Etc/UTC \
+  -e TRANSMISSION_WEB_HOME= `#optional` \
+  -e USER= `#optional` \
+  -e PASS= `#optional` \
+  -e WHITELIST= `#optional` \
+  -e PEERPORT= `#optional` \
+  -e HOST_WHITELIST= `#optional` \
+  -p 9091:9091 \
+  -p 51413:51413 \
+  -p 51413:51413/udp \
+  -v /var/tmp/docker_transmission_mount/data:/config \
+  -v /var/tmp/docker_transmission_mount/downloads:/downloads \
+  -v /var/tmp/docker_transmission_mount/watch:/watch \
+  --restart unless-stopped \
+  transmission
